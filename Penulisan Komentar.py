79c38e8668bcfb084faa68447d0874f3fdb1565e
AhmedLubiz <lubisandra31@gmail.com>
2024-09-27 10:17:33 +0700

    Add files via upload

---
+++ Penulisan Komentar.py
@@ -0,0 +1,9 @@
+# variabel a mereprentasikan panjang
+a = 5
+b = 10 # variabel b merepresentasikan tinggi
+"""
+Dan variabel c merepreasentasikan luas
+persegi dari hasil perkalian
+variabel a dan variabel b
+"""
+c = a * b 
