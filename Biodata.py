429a55a971cf00f936822a4f5e8f18c9b01525f8
AhmedLubiz <lubisandra31@gmail.com>
2024-09-27 10:55:29 +0700

    Add files via upload

---
+++ Biodata.py
@@ -0,0 +1,11 @@
+nama_depan = 'Ahmed'
+nama_belakang = 'Lubis'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = "17"
+alamat = "Pangkalpinang"
+kata_mutiara = "Jika anda bisa memimpikannya, cobalah wujukan jadi kenyataan"
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
