0eb81cd2d261a6379120dc94eec71d883e04f2d6
AhmedLubiz <lubisandra31@gmail.com>
2024-10-11 09:33:20 +0700

    Add files via upload

---
+++ Operator penugasan.py
@@ -0,0 +1,28 @@
+# penugasan pertama
+a = 8
+print('a = 8 -> ', a)
+
+a += 3
+print('a += 3 -> ', a)
+
+a -= 1
+print('a -= 1 -> ', a)
+
+a *= 4
+print('a *= 4 -> ', a)
+
+a /= 8
+print('a /= 8 -> ', a)
+
+
+# karena a jadi float, kita ubah lagi menjadi integer
+a = int(a)
+
+a %= 7
+print('a %= 7 -> ', a)
+
+a //= 4
+print('a //= 4 -> ', a)
+
+a **= 2
+print('a **= 2 -> ', a)
