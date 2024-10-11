8640db82ac8a2ba0172b30d78b75ac6042eed2e1
AhmedLubiz <lubisandra31@gmail.com>
2024-10-11 09:34:28 +0700

    Add files via upload

---
+++ Operator Keanggotaan.py
@@ -0,0 +1,27 @@
+perusahaan = 'Microsoft'
+list_pulau = ['Sumatera', 'Jawa', 'Kalimantan']
+
+# ini adalah dictionary, insyaallah akan kita pelajari
+# di pertemuan-pertemuan yang akan datang
+mahasiswa = {
+    'nama': 'Ahmed Diandra Irdiansyah Lubis',
+    'asal': 'Pangkalpinang'
+}
+
+print(
+"Apakah 'c' ada di variabel perusahaan?",
+'c' in perusahaan
+)
+print(
+"Apakah 'z' tidak ada di variabel perusahaan?",
+'c' not in perusahaan
+)
+
+print(
+    "Apakah 'Medan' tidak ada di variabel list_pulau?",
+    'Medan' not in perusahaan
+)
+print(
+    "Apakah atribut 'nama' ada di variabel mahasiswa?",
+    'nama' in mahasiswa
+)
